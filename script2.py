70415e7a423e1d4b34541fcc523364688ca19b88
weed <Weed-Nim@users.noreply.github.com>
2017-08-09 09:25:53 -0600

    v 0.1.1

--- script2.py
+++ script2.py
@@ -1,1 +1,40 @@
+#------------- Inician Imports. -------------#
+import re
+import sys
+import time
+import random
+import datetime
+from datetime import timedelta
+import calendar
+from calendar import monthrange
+import os
+import pymssql
+import _mssql
+from decimal import *
+import statistics as stats
+#------------- Finalizan Imports. -------------#
 
+#------------- Inicia Declaración de Variables Globales. -------------#
+#------------- Inicia Configuración de BD. -------------#
+#--- Variables de conexión a la base de datos.
+connection = pymssql.connect(server='66.232.22.196',
+                            user='FOXCLEA_TAREAS',
+                            password='JACINTO2014',
+                            database='FOXCLEA_TAREAS'
+                            #charset='utf8mb4',
+                            #cursorclass=pymssql.cursors.DictCursor
+                           )
+#---
+#------------- Finaliza Configuración de BD. -------------#
+
+#------------- Inicio de las funciones Generales -------------#
+#---
+
+#---
+#------------- Fin de las funciones Generales -------------#
+
+#------------- Inicio de declaración de variables -------------#
+#---
+
+#---
+#------------- Fin de declaración de variables -------------#
